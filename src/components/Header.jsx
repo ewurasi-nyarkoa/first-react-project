249c01ac3a54b46a0c554e530b8de49b7df27750
Samuel Bright Bakah <samuelbrightsk@gmail.com>
2024-10-06 21:30:18 +0000

    added a button lorem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,10 @@ const Header = () => {
     <>
     
     <h1>Welcome Home</h1>
-    
+
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit placeat impedit enim tempora voluptatibus, doloribus et ratione quidem accusamus fugit cum id minima labore natus quos itaque. Laborum, possimus illo.</p>
+
+    <button>Lorem</button>
     
     </>
   )
